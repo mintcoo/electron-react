aac9b85c4a9402a5c05d2e412bba65083d62be7e
mintcoo <109326433+mintcoo@users.noreply.github.com>
2024-06-11 00:36:22 +0900

    데이터 2개 읽기

--- src/renderer/components/Home.tsx
+++ src/renderer/components/Home.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import { read, utils } from 'xlsx';
 
 function Home() {
-  const [uploadedFileData, setUploadedFileData] = useState<any[]>([]);
+  const [uploadedOneFileData, setUploadedOneFileData] = useState<any[]>([]);
+  const [uploadedTwoFileData, setUploadedTwoFileData] = useState<any[]>([]);
 
-  const readExcel = async (file: File) => {
+  const readExcel = async (file: File, type: string) => {
     const ab = await file.arrayBuffer();
 
     /* parse */
@@ -12,26 +13,59 @@ function Home() {
 
     const ws = wb.Sheets[wb.SheetNames[0]];
     const data: any[] = utils.sheet_to_json<any>(ws);
-
-    setUploadedFileData(data);
+    console.log(data, 'ㅇㅇ');
+    switch (type) {
+      case 'one':
+        setUploadedOneFileData(data);
+        break;
+      case 'two':
+        setUploadedTwoFileData(data);
+        break;
+      default:
+        console.log('노데이터');
+    }
   };
 
-  const handleExcelFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleExcelFileChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    type: string,
+  ) => {
     if (!e.target.files) return;
     const file = e.target.files[0];
-    readExcel(file);
+    readExcel(file, type);
   };
 
   return (
     <div>
       <h1 className="text-center text-white bg-red-300">엑셀 업로드 테스트</h1>
-      <input type="file" id="excelFile" onChange={handleExcelFileChange} />
-
+      <input
+        type="file"
+        id="excelFile"
+        onChange={(event) => {
+          handleExcelFileChange(event, 'one');
+        }}
+      />
       <div className="flex">
-        {uploadedFileData.map((el, index) => {
+        {uploadedOneFileData.map((el, index) => {
           return (
             <div key={index} className="text-black min-w-20">
               {el.이름}
+            </div>
+          );
+        })}
+      </div>
+      <input
+        type="file"
+        id="excelFile"
+        onChange={(event) => {
+          handleExcelFileChange(event, 'two');
+        }}
+      />
+      <div className="flex">
+        {uploadedTwoFileData.map((el, index) => {
+          return (
+            <div key={index} className="text-black min-w-20">
+              {el.직급}
             </div>
           );
         })}
